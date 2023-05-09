e91410292ee99864d6ede2029025500cfc5f9458
kien-to <ktto@umass.edu>
2023-05-09 15:41:40 -0400

    Fix s

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -7,75 +7,28 @@ import Rejected from "./rejected";
 import Pending from "./pending";
 import NotSubmitted from "./notsubmitted";
 import { useQuery } from "react-query";
-import { sectionDetails } from "../../services/apiSection";
+import { managerPool, sectionDetails } from "../../services/apiSection";
 import { getProfile } from "../../services/apiLogin";
 
 export default function Sections(this: any) {
-//   const [classes, setClasses] = useState([]);
-//   const [isClassesLoading, setClassesLoading] = useState(true);
-//   const [profile, setProfile] = useState({ id: "" });
-//   const [isProfileLoading, setProfileLoading] = useState(true);
 
-  const classes = useQuery({ queryKey: ["student-sections"], queryFn: sectionDetails }).data;
-  const profile = useQuery({ queryKey: ["student-details"], queryFn: getProfile }).data;
+  const classes = useQuery({ queryKey: ["student-sections"], queryFn: managerPool });
+  const profile = useQuery({ queryKey: ["student-details"], queryFn: getProfile });
  
-//   useEffect(() => {
-//     getSections();
-//     getProfile();
-//   }, []);
-
-//   function getSections() {
-//     axios
-//       .get("http://localhost:3000/manager-pool", {
-//         headers: {
-//           Authorization: "Bearer " + Cookies.get("access_token"),
-//         },
-//       })
-//       .then(
-//         (response) => {
-//           let data = response.data;
-//           console.log(data);
-//           setClasses(data);
-//           setClassesLoading(false);
-//           //console.log(Cookies.get('access_token'));
-//         },
-//         (error) => {
-//           console.log(error);
-//         }
-//       );
-//   }
-
-//   function getProfile() {
-//     axios
-//       .get("http://localhost:3000/profile", {
-//         headers: {
-//           Authorization: "Bearer " + Cookies.get("access_token"),
-//         },
-//       })
-//       .then(
-//         (response) => {
-//           setProfile({ id: response.data.id });
-//           setProfileLoading(false);
-//           console.log(profile);
-//         },
-//         (error) => {
-//           console.log(error);
-//         }
-//       );
-//   }
+  console.log(classes);
 
   if (classes.isLoading || profile.isLoading) {
     return <>Loading</>;
   }
 
-  if (classes.length === 0) {
-    return <div />;
+  if (classes.data.length === 0) {
+    return <>Loading</>;
   }
 
   return (
     <>
       <div className="max-w-lg mx-auto space-y-2">
-        {classes.map((data: any) => (
+        {classes.data.map((data: any) => (
           <StudentSection
             key={data._id}
             section={data.sectionNumber}
