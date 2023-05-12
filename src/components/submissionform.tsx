a2cd79b1c6e541b09bf13c8d01e30afaab3bb0ba
TCarel01 <105895758+TCarel01@users.noreply.github.com>
2023-05-12 16:00:34 -0400

    more UI updates

--- src/components/submissionform.tsx
+++ src/components/submissionform.tsx
@@ -22,8 +22,13 @@ import { Check, Trash } from 'lucide-react'
 import { useQuery } from 'react-query'
 import { getProfile } from '../../services/apiLogin'
 import { sectionDetails, managerPool} from '../../services/apiSection'
+import { MultiSelect } from "react-multi-select-component";
+import { useState } from 'react'
 
 export default function SubmissionForm(props: any) {
+    console.log(props);
+    const options = props.sectionList ? props.sectionList.data.map((e: any) => { return {label: e.sectionNumber, value: e.sectionNumber}}) : [];
+    const [selected, setSelected] = useState([]);
     return (
             <AlertDialog>
                 <AlertDialogTrigger asChild>
@@ -33,8 +38,12 @@ export default function SubmissionForm(props: any) {
                 </AlertDialogTrigger>
                 <AlertDialogContent>
                     <AlertDialogHeader>
-                        <AlertDialogTitle>Create Section</AlertDialogTitle>
+                        <AlertDialogTitle>Submission Form</AlertDialogTitle>
                         <div className="space-y-4">
+                            <div className="text-sm text-cyan-500 dark:text-cyan-400 grid w-full items-center gap-1.5 bg-cyan-900">
+                                <Label htmlFor="Email">Preferred Section: </Label>
+                                <MultiSelect options={options} value={selected} onChange={setSelected} labelledBy={'Select'} className='text-black'/>
+                            </div>
                             <div className="text-sm text-cyan-500 dark:text-cyan-400 grid w-full items-center gap-1.5">
                                 <Label htmlFor="Email">Email</Label>
                                 <Input className="w-full" type="text" id="email" placeholder="" />
@@ -50,15 +59,6 @@ export default function SubmissionForm(props: any) {
                                 <select className='flex h-10 w-full rounded-md border border-cyan-300 bg-transparent py-2 px-3 text-sm placeholder:text-cyan-400 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-cyan-700 dark:text-cyan-50 dark:focus:ring-cyan-400 dark:focus:ring-offset-cyan-900'>
                                     <option className='bg-cyan-900' value='A'>A</option>
                                     <option className='bg-cyan-900'value='Other'>Other</option>
-                                </select>
-                            </div>
-
-                            <div className="text-sm text-cyan-500 dark:text-cyan-400 grid w-full items-center gap-1.5">
-                                <Label htmlFor="Email">Preferred Section: </Label>
-                                <select className='selectpicker flex h-10 w-full rounded-md border border-cyan-300 bg-transparent py-2 px-3 text-sm placeholder:text-cyan-400 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-cyan-700 dark:text-cyan-50 dark:focus:ring-cyan-400 dark:focus:ring-offset-cyan-900'>
-                                    {props.sectionList ? props.sectionList.data.map((e: any) => {
-                                        return (<option className='bg-cyan-900' value={e.sectionNumber}>{e.sectionNumber}</option>)
-                                    }) : ''}
                                 </select>
                             </div>
 
