6fd447b7c7657fc89e98967f55a9bf28da9007d4
TCarel01 <105895758+TCarel01@users.noreply.github.com>
2023-05-16 19:50:47 -0400

    less mean about rejection

--- src/components/rejected.tsx
+++ src/components/rejected.tsx
@@ -2,7 +2,7 @@ export default function Rejected() {
     return (
         <li className="bg-red-100 px-3 py-2 rounded flex justify-between items-center">
             <span className="flex justify-between items-center">
-                Rejected.
+                You were not accepted into this section.
             </span>
         </li>
 );}
