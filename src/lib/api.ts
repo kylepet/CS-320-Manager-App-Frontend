4313599dae4224b0bdd0ecce41d8f794cd9169f5
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-05-16 20:55:46 -0400

    format api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,81 +1,83 @@
 import { AUTH } from "./auth"
 
 function buildBaseHeaders() {
-    const headers = new Headers();
-    const token = AUTH.get();
-    if (token != undefined) {
-        headers.set("Authorization", `Bearer ${token}`)
-    }
+  const headers = new Headers()
+  const token = AUTH.get()
+  if (token != undefined) {
+    headers.set("Authorization", `Bearer ${token}`)
+  }
 
-    return headers;
+  return headers
 }
 
 function get(pathname: string) {
-    const headers = buildBaseHeaders();
+  const headers = buildBaseHeaders()
 
-    return fetch(`http://localhost:3000${pathname}`, {
-        method: "GET",
-        headers
-    })
+  return fetch(`http://localhost:3000${pathname}`, {
+    method: "GET",
+    headers,
+  })
 }
 
 function post(pathname: string, body: Record<string, any>) {
-    const headers = buildBaseHeaders();
-    headers.set("Content-Type", "application/json");
+  const headers = buildBaseHeaders()
+  headers.set("Content-Type", "application/json")
 
-    return fetch(`http://localhost:3000${pathname}`, {
-        method: "POST",
-        headers,
-        body: JSON.stringify(body),
-    })
+  return fetch(`http://localhost:3000${pathname}`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  })
 }
 
 export const API = {
-    async submitStudentApplication(body: any) {
-        const response = await post('/studentApps/submit', body);
-        return await response.json()
-    },
+  async submitStudentApplication(body: {
+    email: string
+    CS320taken: boolean
+    references: Array<string>
+    cs320grade: string
+    preference: Array<string>
+  }) {
+    const response = await post("/studentApps/submit", body)
+    return await response.json()
+  },
 
-    async getAllStudentApplications() {
-        const response = await get('/studentApps/allApplications');
-        return await response.json()
-    },
+  async getAllStudentApplications() {
+    const response = await get("/studentApps/allApplications")
+    return await response.json()
+  },
 
-    async accept(body: {
-        accept: boolean
-        studEmail: string
-        profEmail: string
-      }) {
-        const response = await post('/accept', body);
-        return await response.json()
-    },
+  async accept(body: {
+    accept: boolean
+    studEmail: string
+    profEmail: string
+  }) {
+    const response = await post("/accept", body)
+    return await response.json()
+  },
 
-    async changeCap(body: {
-        capChange: number
-        profEmail: string
-      }) {
-        const response = await post('/changeCap', body);
-        return await response.text()
-    },
+  async changeCap(body: { capChange: number; profEmail: string }) {
+    const response = await post("/changeCap", body)
+    return await response.text()
+  },
 
-    async getSections() {
-        const response = await get('/sections');
-        return await response.json()
-    },
+  async getSections() {
+    const response = await get("/sections")
+    return await response.json()
+  },
 
-    async getManagerPool() {
-        const response = await get('/manager-pool');
-        return await response.json()
-    },
+  async getManagerPool() {
+    const response = await get("/manager-pool")
+    return await response.json()
+  },
 
-    async getProfile() {
-        const response = await get('/profile');
-        return await response.json()
-    },
+  async getProfile() {
+    const response = await get("/profile")
+    return await response.json()
+  },
 
-    async signin(body: any) {
-        const response = await post('/signin', body);
-        return await response.json()
-    }
+  async signin(body: any) {
+    const response = await post("/signin", body)
+    return await response.json()
+  },
 }
-  
