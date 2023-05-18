0c5ff06ded54bce235d2425e8545019573c71081
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-05-18 14:44:44 -0400

    comment API code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,20 @@
 import { AUTH } from "./auth"
 
+// Returns common headers for all API calls.
 function buildBaseHeaders() {
   const headers = new Headers()
+
+  // Check if user is authenticated.
   const token = AUTH.get()
   if (token != undefined) {
+    // If the user is authenticated, add it to the headers.
     headers.set("Authorization", `Bearer ${token}`)
   }
 
   return headers
 }
 
+// Helper function for all GET requests.
 function get(pathname: string) {
   const headers = buildBaseHeaders()
 
@@ -19,8 +24,11 @@ function get(pathname: string) {
   })
 }
 
+// Helper function for all POST requests.
 function post(pathname: string, body: Record<string, any>) {
   const headers = buildBaseHeaders()
+
+  // POST requests for our API are all JSON.
   headers.set("Content-Type", "application/json")
 
   return fetch(`http://localhost:3000${pathname}`, {
@@ -30,7 +38,9 @@ function post(pathname: string, body: Record<string, any>) {
   })
 }
 
+// Full API specification for the backend.
 export const API = {
+  // Submit a student application.
   async submitStudentApplication(body: {
     email: string
     CS320taken: boolean
@@ -42,11 +52,13 @@ export const API = {
     return await response.json()
   },
 
+  // Retrieve all student applications.
   async getAllStudentApplications() {
     const response = await get("/studentApps/allApplications")
     return await response.json()
   },
 
+  // Accept or reject an application.
   async accept(body: {
     accept: boolean
     studEmail: string
@@ -56,26 +68,31 @@ export const API = {
     return await response.json()
   },
 
+  // Change capacity of a section.
   async changeCap(body: { capChange: number; profEmail: string }) {
     const response = await post("/changeCap", body)
     return await response.text()
   },
 
+  // Get all sections.
   async getSections() {
     const response = await get("/sections")
     return await response.json()
   },
 
+  // Get the manager pool.
   async getManagerPool() {
     const response = await get("/manager-pool")
     return await response.json()
   },
 
+  // Get user information.
   async getProfile() {
     const response = await get("/profile")
     return await response.json()
   },
 
+  // Login.
   async signin(body: any) {
     const response = await post("/signin", body)
     return await response.json()
