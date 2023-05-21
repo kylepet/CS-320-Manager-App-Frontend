fb2b8ef4e15e03d6ec245d0831ca970f8b428aa0
smcox789 <smcox@umass.edu>
2023-05-20 20:44:03 -0400

    jest

--- test/login.test.js
+++ test/login.test.js
@@ -3,13 +3,15 @@ import { render, fireEvent, waitFor } from "@testing-library/react"
 import Home from "@/pages/index"
 import { handleSubmit } from "@/pages/index"
 import { useRouter } from "next/router"
-import router from "next/dist/server/router"
+import { API } from "@/lib/api"
 import signin from "@/lib/api"
 
 // Mock the API functions used in the component
-jest.mock("@/lib/api", () => ({
-  signin: jest.fn(),
-}))
+jest.mock("@/lib/api.ts", () => ({
+  API: {
+    signin: jest.fn(),
+  },
+}));
 
 describe("Login", () => {
   it("submits the login form and redirects to the appropriate dashboard", async () => {
@@ -18,36 +20,24 @@ describe("Login", () => {
     const usernameInput = getByLabelText("username")
     const passwordInput = getByLabelText("password")
     const loginButton = getByText("button")
-    const fakeIsStudent = getByText("isStudent")
+    const fakeIsStudent = true;
 
     // Mock the successful login response
-    signin.mockResolvedValueOnce({
-      isStudent: fakeIsStudent
-    })
+    API.signin.mockResolvedValueOnce({});
 
     fireEvent.change(usernameInput, { target: { value: "william.lee@umass.edu" } }) //event executed, target value is the email
-    fireEvent.change(passwordInput, { target: { value: "F1fH9HxRj4sX" } })
+    fireEvent.change(passwordInput, { target: { value: "kiento" } })
     fireEvent.click(loginButton)
 
     //action of handleSubmit will be called when the login button is pressed
     await waitFor(() => {
       expect(handleSubmit).toHaveBeenCalledWith({
         email: "william.lee@umass.edu",
-        password: "F1fH9HxRj4sX",
+        password: "kiento",
       })
     })
 
-
     // Check if the appropriate router.push function was called
-    //in this case the fakeIsStudent is set to true already
-    if (fakeIsStudent) {
-      expect(router.push).toHaveBeenCalledWith({
-        pathname: "/studentdashboard",
-      })
-    } else {
-      expect(router.push).toHaveBeenCalledWith({
-        pathname: "/dashboard",
-      })
-    }
+    expect(useRouter().push).toHaveBeenCalledWith("/studentdashboard");
   })
 })
